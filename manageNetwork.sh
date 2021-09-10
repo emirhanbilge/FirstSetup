d2fc5698a95b90d4a48dfd0e9fdacaaf9dd11a3e
Emirhan Bilge Bulut <36032629+emirhanbilge@users.noreply.github.com>
2021-09-10 14:05:54 +0300

    Add files via upload

---
+++ manageNetwork.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /home/pi/Desktop/EbbScripts/Network/ 
+sudo ./resetNetwork.sh
+
+
+if $1 
+then
+	#parametre yok
+	echo "DHCP"
+	#echo "dhcp olacak hiçbir şey yapılmayacak"
+	
+else
+	#parametre var
+	echo "STATIC IP "
+	chmod u+x netSet.sh
+	sudo ./netSet.sh $1 $2 $3
+fi
+
+cd /home/pi/Desktop/EbbScripts/Network/ 
+sudo ./modbusCrontabCreate.sh
--- networkRunner.py
+++ networkRunner.py
@@ -2,30 +2,21 @@
 import subprocess
 
 
-def resetnetwork():
-    #######################################
-    bashCommand = "cd /home/pi/Desktop/EbbScripts/Network/ && sudo ./resetNetwork.sh" 
+def exec():
+    bashCommand = "chmod u+x manageNetwork.sh" 
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    ####################################
 
 
-def startTrigerModbus():
-    bashCommand = "cd /home/pi/Desktop/EbbScripts/Network/ && sudo ./modbusCrontabCreate.sh "
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
 def sendNetwork(ip , subnetMask , gateway ):
 
-    resetnetwork()
-    bashCommand = "chmod u+x netSet.sh" 
+    exec()
+    bashCommand = "sudo ./manageNetwork.sh" +  ip +" "+ subnetMask +" "+ gateway
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    print("Error code " , error)
-    bashCommand = "sudo ./netSet.sh" +  ip +" "+ subnetMask +" "+ gateway
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    startTrigerModbus()
 
 def writedhcp():
-    resetnetwork() 
-    startTrigerModbus()
+    exec()
+    bashCommand = "sudo ./manageNetwork.sh" 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
