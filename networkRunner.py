4fd504c19d199c506c01129dc0baf91e7d5e8332
Emirhan Bilge Bulut <36032629+emirhanbilge@users.noreply.github.com>
2021-09-10 11:29:19 +0300

    Add files via upload

---
+++ networkRunner.py
@@ -0,0 +1,36 @@
+
+import subprocess
+
+
+def resetnetwork():
+    #######################################
+    bashCommand = "cd /home/pi/EbbScripts/Network/" 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    bashCommand = "sudo ./resetNetwork" 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    ####################################
+
+
+def startTrigerModbus():
+    bashCommand = "cd /home/pi/EbbScripts/Network/" 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    bashCommand = "sudo ./modbusCrontabCreate "
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+def sendNetwork(ip , subnetMask , gateway ):
+
+    resetnetwork()
+    bashCommand = "chmod u+x netSet.sh " 
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print("Error code " , error)
+    bashCommand = "./netSet.sh" +  ip +" "+ subnetMask +" "+ gateway
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+
+def writedhcp():
+    resetnetwork() 
